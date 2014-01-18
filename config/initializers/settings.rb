8350e89ca33167f8fa0747a446769e99fb2b7f32
mike-eddie <michael.eddie@gmail.com>
2014-01-18 23:15:54 +0000

    added AHP to demographics

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,8 +1,9 @@
 # Professions
 	MEDICS = ["Medicine BM5", "Medicine BM4", "Medicine BM6"]
-	NURSES = ["Adult Nursing","Child Nursing","Dual Field Adult & Child"]
+	NURSES = ["Adult Nursing","Child Nursing","Dual Field - Adult and Child", "Dual Field - Adult and Mental Health", "Mental Health Nursing"]
 	PARAMEDICS = ["Paramedic Science"]
-	ALL_PROFESSIONS = MEDICS + NURSES + PARAMEDICS
+	AHPS = ["Midwifery", "Other"]
+	ALL_PROFESSIONS = MEDICS + NURSES + PARAMEDICS + AHPS
 
 # Colours
 	COLOURS = ['alizarin','nephritis', 'emerald', 'river', 'orange', 'wisteria']
